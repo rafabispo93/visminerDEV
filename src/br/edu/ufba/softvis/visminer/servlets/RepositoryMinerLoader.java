fc54d9d1c5f6f8e889d8e56379a63ac4b280c459
DavidPinho <davidpinho08@gmail.com>
2016-05-05 15:02:04 -0300

    Add RepositoryMinerLoader servlet

--- src/br/edu/ufba/softvis/visminer/servlets/RepositoryMinerLoader.java
+++ src/br/edu/ufba/softvis/visminer/servlets/RepositoryMinerLoader.java
@@ -1,27 +1,21 @@
 package br.edu.ufba.softvis.visminer.servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 
 import org.repositoryminer.persistence.Connection;
 
-import com.sun.jersey.spi.container.servlet.ServletContainer;
-import com.sun.jersey.spi.container.servlet.WebConfig;
-
-public class VisminerLoader extends ServletContainer{
+public class RepositoryMinerLoader extends HttpServlet{
 	
 	private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void init(WebConfig webConfig) throws ServletException {
+	public void init() throws ServletException {
 		System.out.println("Configuring Database...");
 		
 		Connection connection = Connection.getInstance();
 		connection.connect("mongodb://localhost:27017", "visminer");
-		
-		super.init(webConfig);
 	}
 
-	@Override
 	public void destroy() {
 		System.out.println("Closing Database...");	
 		super.destroy();
